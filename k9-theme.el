76eb3e1579cc576302aca306a0350b247eb93b10
kohnish <kohnish@gmx.com>
2023-09-12 20:37:32 +0200

    blue terminal

--- k9-theme.el
+++ k9-theme.el
@@ -70,6 +70,19 @@
           ("flagged" :foreground ,tardis)
           ("deleted" :foreground ,taffy :bold t))))
 
+  (custom-set-faces
+   ;; '(term-color-black ((t (:foreground "#3F3F3F" :background "#2B2B2B"))))
+   ;; '(term-color-red ((t (:foreground "#AC7373" :background "#8C5353"))))
+   ;; '(term-color-green ((t (:foreground "#7F9F7F" :background "#9FC59F"))))
+   ;; '(term-color-yellow ((t (:foreground "#DFAF8F" :background "#9FC59F"))))
+   `(term-color-blue ((t (:foreground ,tardis :background ,bg))))
+   ;; '(term-color-magenta ((t (:foreground "#DC8CC3" :background "#CC9393"))))
+   ;; '(term-color-cyan ((t (:foreground "#93E0E3" :background "#8CD0D3"))))
+   ;; '(term-color-white ((t (:foreground "#DCDCCC" :background "#656555"))))
+   ;; '(term-default-fg-color ((t (:inherit term-color-white))))
+   ;; '(term-default-bg-color ((t (:inherit term-color-black))))
+    )
+
   (custom-theme-set-faces
     'k9
 
